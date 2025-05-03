e2dde1ad8fef4637e7cb4e85543e91e283b145b9
tendryAxel <hei.tendry@gmail.com>
2025-05-03 15:52:32 +0300

    chore: add react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <MemoryRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </MemoryRouter>
     </Provider>
   </StrictMode>,
 );
